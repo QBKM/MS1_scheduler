90a74dc9479c5fbf49711ab332b2cf8d01470741
Quentin Bakrim <quentin.bakrim@hotmail.fr>
2021-09-29 15:22:54 +0200

    base

---
+++ Components/Application/API_application.c
@@ -0,0 +1,8 @@
+/*
+ * API_application.c
+ *
+ *  Created on: Sep 29, 2021
+ *      Author: Quent
+ */
+
+
---
+++ Components/Application/inc/API_application.h
@@ -0,0 +1,13 @@
+/*
+ * API_application.h
+ *
+ *  Created on: Sep 29, 2021
+ *      Author: Quent
+ */
+
+#ifndef APPLICATION_INC_API_APPLICATION_H_
+#define APPLICATION_INC_API_APPLICATION_H_
+
+
+
+#endif /* APPLICATION_INC_API_APPLICATION_H_ */
